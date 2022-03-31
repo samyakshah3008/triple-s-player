026ec770f01950f2903a469ea07116e2fe9b7c07
Samyak Shah <samyakshah3008@gmail.com>
2022-03-31 17:13:05 +0530

    initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ export default function Navbar() {
         <nav>
             <div className='header'>Hey there</div>
             <div className='header'>Hey there</div>
+            <div className='header'>Hey there</div>
         </nav>
     </div>
   )
