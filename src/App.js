a79d243c7cd5219a3d887775ab19b38c933ff98e
Samyak Shah <samyakshah3008@gmail.com>
2022-03-31 17:10:42 +0530

    initial setup for folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>Samyak</h1>
       <h1>From development</h1>
     </div>
