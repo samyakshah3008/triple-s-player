0dd872eed91f72917d36341bd044c3f337819e5d
Samyak Shah <samyakshah3008@gmail.com>
2022-03-31 17:11:05 +0530

    initial setup for folder structure

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./navbar.css"
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav>
+            <div className='header'>Hey there</div>
+            <div className='header'>Hey there</div>
+        </nav>
+    </div>
+  )
+}
